9267c3251d59c81c27d890c3053081a24b5fb3af
Jiří Zárevúcky <zarevucky.jiri@gmail.com>
2018-05-02 17:32:27 +0200

    Rebase and squash basic HelenOS support from 7.3.0 branch.

---
+++ gcc/config/helenos-stdint.h
@@ -0,0 +1,54 @@
+/* Definitions for HelenOS.
+   Copyright (C) 1996-2017 Free Software Foundation, Inc.
+   Copyright (C) 2018 CZ.NIC, z.s.p.o.
+
+This file is part of GCC.
+
+GCC is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 3, or (at your option)
+any later version.
+
+GCC is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with GCC; see the file COPYING3.  If not see
+<http://www.gnu.org/licenses/>.  */
+
+#define SIG_ATOMIC_TYPE "int"
+
+#define INT8_TYPE    "signed char"
+#define INT16_TYPE   "short int"
+#define INT32_TYPE   "int"
+#define INT64_TYPE   "long long int"
+#define UINT8_TYPE   "unsigned char"
+#define UINT16_TYPE  "short unsigned int"
+#define UINT32_TYPE  "unsigned int"
+#define UINT64_TYPE  "long long unsigned int"
+
+#define INT_LEAST8_TYPE    INT8_TYPE
+#define INT_LEAST16_TYPE   INT16_TYPE
+#define INT_LEAST32_TYPE   INT32_TYPE
+#define INT_LEAST64_TYPE   INT64_TYPE
+#define UINT_LEAST8_TYPE   UINT8_TYPE
+#define UINT_LEAST16_TYPE  UINT16_TYPE
+#define UINT_LEAST32_TYPE  UINT32_TYPE
+#define UINT_LEAST64_TYPE  UINT64_TYPE
+
+#define INT_FAST8_TYPE    INT8_TYPE
+#define INT_FAST16_TYPE   INT16_TYPE
+#define INT_FAST32_TYPE   INT32_TYPE
+#define INT_FAST64_TYPE   INT64_TYPE
+#define UINT_FAST8_TYPE   UINT8_TYPE
+#define UINT_FAST16_TYPE  UINT16_TYPE
+#define UINT_FAST32_TYPE  UINT32_TYPE
+#define UINT_FAST64_TYPE  UINT64_TYPE
+
+#define INTPTR_TYPE  "long int"
+#define UINTPTR_TYPE "long unsigned int"
+
+#define INTMAX_TYPE  "long long int"
+#define UINTMAX_TYPE "long long unsigned int"
