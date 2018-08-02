7f49bed70ef8d89d9d97232b33276a2f51f51ecb
Jiří Zárevúcky <jiri.zarevucky@nic.cz>
2018-08-02 19:01:54 +0200

    Revert back to glibc-compatible stdint types.

--- gcc/config/helenos-stdint.h
+++ gcc/config/helenos-stdint.h
@@ -20,35 +20,35 @@ along with GCC; see the file COPYING3.  If not see
 
 #define SIG_ATOMIC_TYPE "int"
 
-#define INT8_TYPE    "signed char"
-#define INT16_TYPE   "short int"
-#define INT32_TYPE   "int"
-#define INT64_TYPE   "long long int"
-#define UINT8_TYPE   "unsigned char"
-#define UINT16_TYPE  "short unsigned int"
-#define UINT32_TYPE  "unsigned int"
-#define UINT64_TYPE  "long long unsigned int"
+#define INT8_TYPE "signed char"
+#define INT16_TYPE "short int"
+#define INT32_TYPE "int"
+#define INT64_TYPE (LONG_TYPE_SIZE == 64 ? "long int" : "long long int")
+#define UINT8_TYPE "unsigned char"
+#define UINT16_TYPE "short unsigned int"
+#define UINT32_TYPE "unsigned int"
+#define UINT64_TYPE (LONG_TYPE_SIZE == 64 ? "long unsigned int" : "long long unsigned int")
 
-#define INT_LEAST8_TYPE    INT8_TYPE
-#define INT_LEAST16_TYPE   INT16_TYPE
-#define INT_LEAST32_TYPE   INT32_TYPE
-#define INT_LEAST64_TYPE   INT64_TYPE
-#define UINT_LEAST8_TYPE   UINT8_TYPE
-#define UINT_LEAST16_TYPE  UINT16_TYPE
-#define UINT_LEAST32_TYPE  UINT32_TYPE
-#define UINT_LEAST64_TYPE  UINT64_TYPE
+#define INT_LEAST8_TYPE "signed char"
+#define INT_LEAST16_TYPE "short int"
+#define INT_LEAST32_TYPE "int"
+#define INT_LEAST64_TYPE (LONG_TYPE_SIZE == 64 ? "long int" : "long long int")
+#define UINT_LEAST8_TYPE "unsigned char"
+#define UINT_LEAST16_TYPE "short unsigned int"
+#define UINT_LEAST32_TYPE "unsigned int"
+#define UINT_LEAST64_TYPE (LONG_TYPE_SIZE == 64 ? "long unsigned int" : "long long unsigned int")
 
-#define INT_FAST8_TYPE    INT8_TYPE
-#define INT_FAST16_TYPE   INT16_TYPE
-#define INT_FAST32_TYPE   INT32_TYPE
-#define INT_FAST64_TYPE   INT64_TYPE
-#define UINT_FAST8_TYPE   UINT8_TYPE
-#define UINT_FAST16_TYPE  UINT16_TYPE
-#define UINT_FAST32_TYPE  UINT32_TYPE
-#define UINT_FAST64_TYPE  UINT64_TYPE
+#define INT_FAST8_TYPE "signed char"
+#define INT_FAST16_TYPE (LONG_TYPE_SIZE == 64 ? "long int" : "int")
+#define INT_FAST32_TYPE (LONG_TYPE_SIZE == 64 ? "long int" : "int")
+#define INT_FAST64_TYPE (LONG_TYPE_SIZE == 64 ? "long int" : "long long int")
+#define UINT_FAST8_TYPE "unsigned char"
+#define UINT_FAST16_TYPE (LONG_TYPE_SIZE == 64 ? "long unsigned int" : "unsigned int")
+#define UINT_FAST32_TYPE (LONG_TYPE_SIZE == 64 ? "long unsigned int" : "unsigned int")
+#define UINT_FAST64_TYPE (LONG_TYPE_SIZE == 64 ? "long unsigned int" : "long long unsigned int")
 
-#define INTPTR_TYPE  "long int"
-#define UINTPTR_TYPE "long unsigned int"
+#define INTPTR_TYPE (LONG_TYPE_SIZE == 64 ? "long int" : "int")
+#define UINTPTR_TYPE (LONG_TYPE_SIZE == 64 ? "long unsigned int" : "unsigned int")
 
 #define INTMAX_TYPE  "long long int"
 #define UINTMAX_TYPE "long long unsigned int"
