bcc0c92b1a6e58d3183b793874fa20d0c129a15c
Jiří Zárevúcky <zarevucky.jiri@gmail.com>
2019-01-09 18:35:14 +0100

    Fix native MIPS compiler
    This is the same issue that the coastline GCC 4.6 has.
    The fix currently doesn't affect anything, since we don't
    cross-compile a native gcc from this repo yet.

--- gcc/config/mips/helenos.h
+++ gcc/config/mips/helenos.h
@@ -93,19 +93,8 @@ along with GCC; see the file COPYING3.  If not see
 # define NO_SHARED_SPECS ""
 #endif
 
-/* -march=native handling only makes sense with compiler running on
-   a MIPS chip.  */
-#if defined(__mips__)
-extern const char *host_detect_local_cpu (int argc, const char **argv);
-# define EXTRA_SPEC_FUNCTIONS \
-  { "local_cpu_detect", host_detect_local_cpu },
-
-# define MARCH_MTUNE_NATIVE_SPECS				\
-  " %{march=native:%<march=native %:local_cpu_detect(arch)}"	\
-  " %{mtune=native:%<mtune=native %:local_cpu_detect(tune)}"
-#else
-# define MARCH_MTUNE_NATIVE_SPECS ""
-#endif
+/* We don't currently support native cpu detection on HelenOS.  */
+#define MARCH_MTUNE_NATIVE_SPECS ""
 
 #define LINUX_DRIVER_SELF_SPECS \
   NO_SHARED_SPECS							\
