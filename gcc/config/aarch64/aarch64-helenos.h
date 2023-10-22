e6e4dc1ced25380b2ef8cdb5a8cfd579b12327c5
Jiří Zárevúcky <zarevucky.jiri@gmail.com>
2023-10-22 23:10:13 +0200

    AArch64 errata are now in a separate header

--- gcc/config/aarch64/aarch64-helenos.h
+++ gcc/config/aarch64/aarch64-helenos.h
@@ -31,25 +31,7 @@
    -X						\
    %{mbig-endian:-EB} %{mlittle-endian:-EL}"
 
-#if TARGET_FIX_ERR_A53_835769_DEFAULT
-#define CA53_ERR_835769_SPEC \
-  " %{!mno-fix-cortex-a53-835769:--fix-cortex-a53-835769}"
-#else
-#define CA53_ERR_835769_SPEC \
-  " %{mfix-cortex-a53-835769:--fix-cortex-a53-835769}"
-#endif
-
-#if TARGET_FIX_ERR_A53_843419_DEFAULT
-#define CA53_ERR_843419_SPEC \
-  " %{!mno-fix-cortex-a53-843419:--fix-cortex-a53-843419}"
-#else
-#define CA53_ERR_843419_SPEC \
-  " %{mfix-cortex-a53-843419:--fix-cortex-a53-843419}"
-#endif
-
-#define LINK_SPEC HELENOS_TARGET_LINK_SPEC \
-                  CA53_ERR_835769_SPEC \
-                  CA53_ERR_843419_SPEC
+#define LINK_SPEC HELENOS_TARGET_LINK_SPEC AARCH64_ERRATA_LINK_SPEC
 
 #define TARGET_ASM_FILE_END file_end_indicate_exec_stack
 
